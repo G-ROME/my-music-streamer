3eb0e37cbdb8866f00b8fa956e6edc42d43a20dc
G-ROME <josejeromelalunio@gmail.com>
2020-09-16 18:49:48 +0800

    Updates

---
+++ precache-manifest.2d1fab5097465c4dff2c5c22b363697e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "442f483d097453b3fde1bfc54f77e906",
+    "url": "/my-music-streamer/index.html"
+  },
+  {
+    "revision": "6a47af2d4624af408d47",
+    "url": "/my-music-streamer/static/css/main.18548e97.chunk.css"
+  },
+  {
+    "revision": "9d4a8c792a2ae2b8b3a8",
+    "url": "/my-music-streamer/static/js/2.a1615aaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-music-streamer/static/js/2.a1615aaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a47af2d4624af408d47",
+    "url": "/my-music-streamer/static/js/main.be4447b4.chunk.js"
+  },
+  {
+    "revision": "25280051ab2dbd0ef9bb",
+    "url": "/my-music-streamer/static/js/runtime-main.ef03f739.js"
+  }
+]);
